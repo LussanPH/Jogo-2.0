21d0a37946a4e7d3f5be2f1dec01a47de73009b2
Ismael Sousa <165102646+ISMAELSOUSAFONTELESCASTRO@users.noreply.github.com>
2024-09-15 17:19:16 -0300

    Update JogadorComum.java

--- src/jogadores/JogadorComum.java
+++ src/jogadores/JogadorComum.java
@@ -7,12 +7,6 @@ public class JogadorComum extends Jogador{
 		this.casa = 0;
 	}
 	
-	public JogadorComum(String cor, int casa, int pontuacao) {
-		this.cor = cor;
-		this.casa = casa;
-		this.pontuacao = pontuacao;
-	}
-
 	public void JogarDados() {
 		dado[0].JogarDado();
 		dado[1].JogarDado();
