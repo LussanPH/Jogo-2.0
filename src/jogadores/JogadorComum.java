2e04fff148acd751e1265f57b5ab2090d4d8c74e
LussanPH <166248501+LussanPH@users.noreply.github.com>
2024-09-16 00:57:42 -0300

    Coloquei só o tipo comum

--- src/jogadores/JogadorComum.java
+++ src/jogadores/JogadorComum.java
@@ -5,9 +5,11 @@ public class JogadorComum extends Jogador{
 	public JogadorComum(String cor) {
 		this.cor = cor;
 		this.casa = 0;
+		tipo = TipoDeJogador.COMUM;
 	}
-	
+
 	public void JogarDados() {
+		intanciadoDados();
 		dado[0].JogarDado();
 		dado[1].JogarDado();
 	}
