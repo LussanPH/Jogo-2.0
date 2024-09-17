5154d643f3c2089255b4a0e319b789ee561ff993
LussanPH <166248501+LussanPH@users.noreply.github.com>
2024-09-17 15:54:14 -0300

    adicionei lista de jogadores ao aplicar regra

--- src/casa/Casa.java
+++ src/casa/Casa.java
@@ -1,9 +1,10 @@
 package casa;
 import jogadores.*;
+import java.util.List;
 
 public abstract class Casa {
 	public int numero;
 	public TipoDeCasa tipo;
-	public abstract Jogador aplicarRegra(Jogador jogador);
+	public abstract Jogador aplicarRegra(Jogador jogador, List<Jogador> jogadores);
 	
 }
