e74485839b6dc734cb644a68807e194427c9ed4d
ismael <ismaelsousafcastro@gmail.com>
2024-09-17 17:58:22 -0300

    .

--- src/casa/CasaAzar.java
+++ src/casa/CasaAzar.java
@@ -2,6 +2,8 @@ package casa;
 
 import jogadores.Jogador;
 import jogadores.JogadorSortudo;
+import jogadores.TipoDeJogador;
+
 import java.util.List;
 
 import exceptions.IllegalMoneyException;
@@ -15,12 +17,12 @@ public class CasaAzar extends Casa{
 
 	@Override
 	public Jogador aplicarRegra(Jogador jogador, List<Jogador> jogadores) {
-		if (!(jogador instanceof JogadorSortudo)) {
+		if (!(jogador.getTipo() == TipoDeJogador.SORTUDO)) {
 			try {
 				jogador.setMoedas(jogador.getMoedas() - 3);	
 			}catch(IllegalMoneyException ime) {
 				try {
-					jogador.setMoedas(jogador.getMoedas());
+					jogador.setMoedas(0);
 				} catch (IllegalMoneyException e) {
 					e.printStackTrace();
 				}
